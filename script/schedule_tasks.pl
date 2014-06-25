7aff27846d5a745582f0fe24a84204e2b0110d5d
rhalliday <rob_halliday_1@hotmail.com>
2014-06-25 11:52:37 +0100

    remove server check

--- script/schedule_tasks.pl
+++ script/schedule_tasks.pl
@@ -10,18 +10,6 @@ my $socket = IO::Socket::INET->new(
     PeerPort => q{4015},
 );
 
-unless($socket) {
-    system(q{/etc/init.d/scheduled-tasks restart}) == 0
-        or die q{Unable to start scheduled tasks};
-
-    sleep(2);
-
-    $socket = IO::Socket::INET->new(
-        PeerHost => q{localhost},
-        PeerPort => q{4015},
-    ) or die q{ERROR in Socket Creation : },$!,qq{\n};
-}
-
 $socket->send(q{run task} . qq{\n});
 sleep(1);
 $socket->close();
