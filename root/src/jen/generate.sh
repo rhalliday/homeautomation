dbfcd25529843b47c4ded31a7adf15d7f9ffefce
rhalliday <rob_halliday_1@hotmail.com>
2014-08-14 23:49:10 +0100

    remove the need for javascript by placing everything in template files and generating the html files for it.

---
+++ root/src/jen/generate.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+tpage index.tt2 > ../../../index.html 
+tpage about.tt2 > ../../../about.html
+tpage testimonials.tt2 > ../../../testimonials.html
+tpage contact.tt2 > ../../../contact.html
